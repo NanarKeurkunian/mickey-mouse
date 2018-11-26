8db8edce574f58547a246b61fd7833e48951358f
NanarKeurkunian <45368469+NanarKeurkunian@users.noreply.github.com>
2018-11-26 13:31:07 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,56 @@
+function setup() {
+	createCanvas(600, 600);
+	background(0,200,255);
+
+}
+
+function draw() {
+	fill(255,255,0);
+	triangle(250,260,300,100,350,260);
+	fill(255,0,255);
+	stroke(255,0,255);
+	ellipse(280,228,30,30);
+	ellipse(320,228,30,30);
+	ellipse(290,210,20,20);
+	ellipse(320,195,20,20);
+	ellipse(295,155,23,23);
+	ellipse(300,180,30,30);
+	triangle(280,100,300,140,320,100);
+	triangle(280,120,300,90,320,120);
+	fill(0);
+	stroke(0);
+	ellipse(300,400,300,312);
+	ellipse(460,235,200,212);
+	ellipse(140,235,200,212);
+	ellipse(300,520,135,100);
+	fill(210,180,140);
+	stroke(210,180,140);
+	ellipse(258,390,120,250);
+	ellipse(342,390,120,250);
+	ellipse(300,500,190,100);
+	ellipse(370,490,70,70);
+	ellipse(230,490,70,70);
+	ellipse(220,480,70,70);
+	ellipse(380,480,70,70);
+	ellipse(210,470,70,70);
+	ellipse(390,470,70,70);
+	ellipse(300,515,135,100);
+	fill(255);
+	stroke(255);
+	ellipse(260,360,50,110);
+	ellipse(340,360,50,110);
+	fill(0);
+	stroke(0);
+	ellipse(265,385,30,70);
+	ellipse(335,385,30,70);
+	fill(0);
+	stroke(0);
+	ellipse(300,450,90,60)
+	noFill();
+	stroke(0);
+	arc(300,345,310,350,.92,-180);
+	arc(300,430,150,250,.92,-180);
+	fill(210,50,100)
+	stroke(0)
+	ellipse(300,545,50,20);
+}
